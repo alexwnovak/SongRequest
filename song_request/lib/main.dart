79d861f870a9971cd8e058ee6622648188000dc4
Alex Novak <alexwnovak@gmail.com>
2023-05-25 22:07:37 -0400

    Adding a scaffold

--- song_request/lib/main.dart
+++ song_request/lib/main.dart
@@ -45,7 +45,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: const Scaffold(
+        body: MyHomePage(),
+      ),
     );
   }
 }
