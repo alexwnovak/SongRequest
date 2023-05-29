0029ea3d7fc0e4235d2c5ce7b0253f9c15bfe96b
Alex Novak <alexwnovak@gmail.com>
2023-05-29 13:43:43 -0400

    Updating SongPool to match
    I think Song Pool isn't the right name, since it's not gonna be a pool.
    That is, it won't be a source we draw from, but instead will just be the
    raw requests that we'll still pre-populate... but then increment the
    votes.

--- common/lib/song_pool.dart
+++ common/lib/song_pool.dart
@@ -1,16 +1,22 @@
 class SongPool {
   final String sessionId;
-  final List<int> songIds;
+  final int songId;
+  final bool wasPlayed;
+  final int requests;
 
   SongPool({
     required this.sessionId,
-    required this.songIds,
+    required this.songId,
+    required this.wasPlayed,
+    required this.requests,
   });
 
   factory SongPool.fromMap(Map<String, dynamic> data) {
     return SongPool(
       sessionId: data['sessionId'],
-      songIds: data['songs'].toString().split(',').map((e) => int.parse(e)).toList(),
+      songId: data['songId'],
+      wasPlayed: data['wasPlayed'] as bool,
+      requests: data['requests'] as int,
     );
   }
 }
