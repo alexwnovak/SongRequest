e5a5d3b61a65fe142c53e87a23a0e4a99ee94598
Alex Novak <alexwnovak@gmail.com>
2023-05-28 20:08:58 -0400

    Expanding the Song object to contain more data

--- common/lib/song.dart
+++ common/lib/song.dart
@@ -1,11 +1,25 @@
 class Song {
-  final String id;
+  final int id;
   final String artist;
   final String title;
+  final String album;
+  final int year;
 
   Song({
     required this.id,
     required this.artist,
     required this.title,
+    required this.album,
+    required this.year,
   });
+
+  factory Song.fromMap(Map<String, dynamic> data) {
+    return Song(
+      id: data['id'] as int,
+      artist: data['artist'],
+      title: data['title'],
+      album: data['album'],
+      year: data['year'] as int,
+    );
+  }
 }
